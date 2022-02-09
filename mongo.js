519c4283f997a94a64cfc9d9fc8d051105919f43
lucianooieni <lucianooieni21@gmail.com>
2022-02-08 22:07:02 -0300

    little change

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const { MONGODB_URI } = process.env
+// const { MONGODB_URI } = process.env
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect('mongodb+srv://lucianooieni:luciano0682@cluster0.kapok.mongodb.net/app_notes?retryWrites=true&w=majority')
   .then(result => console.log('connected to MongoDB'))
   .catch(error => console.log('error connecting to MongoDB:', error.message))
